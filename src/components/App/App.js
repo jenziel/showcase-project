4c7dd75e25dec527b0071f239c748ada6c074e2c
Jen Ziel <jenziel400@gmail.com>
2023-09-19 19:40:39 -0400

    Add basic header component.  Add variables to control state for error, loading state, collection state, singular artwork state. Add cute font to mimic 2757GT America2757. Add apiCalls for fetching the collection and singular piece.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import LoadingComponent from '../LoadingComponent/LoadingComponent';
+import { fetchCollection, fetchSelectedArtwork } from '../../apiCalls';
+import Header from '../Header/Header'
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [collection, setCollection] = useState([]);
+  const [newError, setError] = useState(null); 
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedPiece, setSelectedPiece] = useState(null);
+
+  function getCollectionInfo(){
+    fetchCollection()
+    .then((data)=>{
+      setCollection(data);
+      setIsLoading(false);
+    })
+    .catch((response) => {
+      setError(response || "failed to fetch collection!");
+    });
+  }
+  useEffect(()=>{
+    getCollectionInfo()
+  }, [])
+
   return (
     <div className="App">
+      <Header />
       {/* <header className="App-header">
       </header> */}
       <LoadingComponent/>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+      <header className='App-header'>
+        <div className='App-logo'>
+          <h1>l o g o </h1>
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header;
